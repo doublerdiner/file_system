cf7929802dfc53151f98f14de3498f08b70f59f9
Chris Barclay <chris.barclay1@gmail.com>
2023-04-25 16:04:23 +0100

    Initial Commit

---
+++ src/test/java/com/codeclan/filesystemservice/FilesystemserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.filesystemservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilesystemserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
